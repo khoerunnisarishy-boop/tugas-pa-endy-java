3e6f0f6e49ff60ff5f0365d0e21109f0fa34944d
khoerunnisarishy-boop <khoerunnisarishy@gmail.com>
2025-11-07 09:53:39 +0700

    Add files via upload

---
+++ Halo.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class Halo {
+    public static void main(String[] args) {
+        System.out.println("Halo Rishy Cantik");
+        System.out.println("Waktu saat ini :"+LocalDateTime.now());
+    }
+}
+    
